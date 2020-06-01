eb12617396384420d33a05bd881442534a603b7b
utautattaro <tsudaryo@utautattaro.com>
2020-06-01 14:59:25 +0900

    upload, bootstrap, saving

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,12 +1,24 @@
 window.addEventListener("load", () => {
 	const canvas = document.querySelector("#canvas");
 	const clearButton = document.querySelector("#clear");
-	const blackButton = document.querySelector("#black");
-	const whiteButton = document.querySelector("#white");
+	const saveButton = document.querySelector("#savePic");
+	const imgSource = document.querySelector("#exampleFormControlFile1");
+	const output = document.querySelector("#output");
 	const ctx = canvas.getContext('2d');
 
 	const img = new Image();
-	img.src = "assets/images/IMG_1427.JPG";
+	// img.src = "assets/images/IMG_1427.JPG";
+
+	imgSource.addEventListener('change', (e) => {
+		console.log(imgSource);
+		console.log(e);
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			img.src = e.target.result;
+		}
+		reader.readAsDataURL(e.target.files[0]);
+		$('#drawmodal').modal({"backdrop":"static"});
+	});
 
 	img.onload = () => {
 		const [img_scaled_width, img_scaled_height] = drawImageToScale(img, ctx);
@@ -21,6 +33,7 @@ window.addEventListener("load", () => {
 	canvas.addEventListener("mousemove", draw);
 
 	let painting = false;
+	let imgsize;
 
 	function startPosition(e){
 		painting = true;
@@ -35,22 +48,35 @@ window.addEventListener("load", () => {
 	function draw(e){
 		if (!painting)
 			return;
-		ctx.lineWidth = 3;
+		ctx.lineWidth = 10;
 		ctx.lineCap = 'round';
-		ctx.lineTo(e.clientX, e.clientY);
+		ctx.strokeStyle = "#000000";
+
+		let pos = getMousePos(canvas, e);
+
+		ctx.lineTo(pos.x, pos.y);
 		ctx.stroke();
 		ctx.beginPath();
-		ctx.moveTo(e.clientX, e.clientY);
+		ctx.moveTo(pos.x, pos.y);
+	}
+
+	function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+		  x: evt.clientX - rect.left,
+		  y: evt.clientY - rect.top
+		};
 	}
 
 	clearButton.addEventListener('click', () => clearCanvas(img, ctx, canvas.width, canvas.height));
-	blackButton.addEventListener('click', () => ctx.strokeStyle = "#000000");
-	whiteButton.addEventListener('click', () => ctx.strokeStyle = "#ffffff");
+	saveButton.addEventListener('click', () => saveCanvas(canvas,output));
+	// blackButton.addEventListener('click', () => );
+	// whiteButton.addEventListener('click', () => ctx.strokeStyle = "#ffffff");
 
 });
 
 function drawImageToScale(img, ctx){
-	const img_width = 650;
+	const img_width = 400;
 	const scaleFactor = img_width / img.width;
 	const img_height = img.height * scaleFactor;
 	ctx.drawImage(img, 0 , 0, img_width, img_height);
@@ -60,4 +86,10 @@ function drawImageToScale(img, ctx){
 function clearCanvas(img, ctx, img_scaled_width, img_scaled_height){
 	ctx.clearRect(0, 0, img_scaled_width, img_scaled_height);
 	drawImageToScale(img,ctx);
+}
+
+function saveCanvas(canvas , outputDOM){
+	let base64 = canvas.toDataURL();
+	outputDOM.src = base64;
+	$('#drawmodal').modal('hide');
 }
